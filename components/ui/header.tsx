ae9a2b32057877b2801110ba2a2ec4081edba0ac
Akash Das <akashdas0599@gmail.com>
2025-12-06 18:11:26 +0530

    Refactor global styles and layout; add header component. Update color variables in globals.css, replace Geist font with Poppins in layout.tsx, and implement a new Header component. Remove unused SVG files and clean up the Home page.

---
+++ components/ui/header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="bg-foreground flex">
+      <Image src="/images/logo.svg" alt="Amrutam" width={67} height={67} />
+      <div className="flex items-center justify-between grow">
+        <Image
+          src="/images/amrutam-logo.png"
+          alt="Amrutam"
+          width={164}
+          height={46}
+        />
+        <div className="flex items-center gap-3 mr-[66px] text-right">
+          <div>
+            <div className="text-black font-semibold text-base">John Doe</div>
+            <div className="text-gray font-normal text-[12px]">Admin</div>
+          </div>
+          <Image
+            src="/images/admin-avatar.png"
+            alt="Admin Avatar"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
